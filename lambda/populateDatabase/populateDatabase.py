aec52e0fb81da1d42f6a4125ed67c115451696eb
master-harvey <ianpadraicharvey@gmail.com>
2024-04-29 13:26:20 -0600

    correct link to repo

--- lambda/populateDatabase/populateDatabase.py
+++ lambda/populateDatabase/populateDatabase.py
@@ -25,7 +25,7 @@ class HTMLFilter(HTMLParser):
 def handler(event,context):
     """This function will run regularly and will poll for updates to the wiki and update or add the embeddings"""
     # Request any updates from the last 24 hours (forbidden without a user agent)
-    user_agent = "DenhacWikiBot/1.0 (+https://github.com/master-harvey/wiki-bot)"
+    user_agent = "DenhacWikiBot/1.0 (+https://github.com/master-harvey/wikibot)"
     PAST_WEEK = (datetime.datetime.now() - datetime.timedelta(days=7))
     with requests.get(f"https://denhac.org/wp-json/wp/v2/epkb_post_type_1?modified_after={PAST_WEEK.isoformat()}", headers={'User-Agent': user_agent}) as response:
         # Read the response data
