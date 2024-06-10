7ae1f6f068036041f27fc56b8467e1e25b2f55aa
master-harvey <ianpadraicharvey@gmail.com>
2024-06-09 18:09:47 -0600

    retrieval from atlas working as expected

--- lambda/generateReply/generateReply.py
+++ lambda/generateReply/generateReply.py
@@ -17,29 +17,32 @@ query_model_string = 'meta-llama/Llama-3-8b-chat-hf' # model API string from Tog
 
 def handler(event,context):
     """The lambda handler that will actually query the database and send a reply over Slack"""
-    event = loads(event['body']) #convert plain text json to python dictionary
+    try:
+      event = loads(event['body']) #convert plain text json to python dictionary
+    except:
+      event = event['body']
 
     # Query the database https://www.mongodb.com/docs/atlas/atlas-vector-search/vector-search-stage/
     message_embedding = AI.embeddings.create(input=event['message'], model=embedding_model_string).data[0].embedding
     
-    search_results = mongo.Denhac_Wiki.Articles.aggregate([
+    search_results = list(mongo.Denhac_Wiki.Articles.aggregate([
       {
         "$vectorSearch": {
+          "index": "vector_embeddings",
           "queryVector": message_embedding,
           "path": vector_database_field_name,
           "numCandidates": 45, # this should be 10-20x the limit
           "limit": 3, # the number of documents to return in the results
-          "index": vector_database_field_name, # the index name you used in Step 4.
         } # https://docs.together.ai/docs/inference-models
       } # The largest articles have ~2100 tokens so an 8k context model should accurately interpret 3 articles + the user's query
-    ])
+    ]))
 
     # BIG AI STUFF
     prompt = "You are a helpful digital assistant made to answer questions about the Denhac Hackerspace. "
     "Based on the user's query and relevant articles from the hackerspace wiki, provide the most complete, helpful, and concise answer possible. "
     "If the prompt pertains to Denhac's facilities but the answer is not contained in the wiki, do not try to guess the correct information about Denhac and instead inform the user that you do not know. "
     "<User's Query>\n" + event['message'] + "\n</User's Query>\n "
-    "<Wiki Articles>\n" + "\n\n----------\n\n".join(["Title: " + article['title'] + "\n" + article['content'] for article in search_results]) + "</Wiki Articles>\n"
+    "<Wiki Articles>\n" + "\n\n----------\n\n".join(["Title: " + article['title']['rendered'] + "\n" + article['content'] for article in search_results]) + "</Wiki Articles>\n"
     # TODO: Fill the remaining context window with the preceding conversation, without this it won't feel like a 'chatbot'
     
     response = AI.Complete.create(
